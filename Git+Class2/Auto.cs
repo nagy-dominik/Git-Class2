125658e2985112dddc7a89082e0f51338218c339
Nagy Dominik <pwiproduct@gmail.com>
2020-02-17 09:28:39 +0100

    Basic Concept

---
+++ Git+Class2/Auto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Class2
+{
+    class Auto
+    {
+        private string _marka;
+        private string _modell;
+        private int _ev;
+        private int _kilometer;
+        private string _uzemanyag;
+        private int _ar;
+
+        public Auto(string marka, string modell, int ev, int kilometer, string uzemanyag, int ar)
+        {
+            Marka = marka;
+            Modell = modell;
+            Ev = ev;
+            Kilometer = kilometer;
+            Uzemanyag = uzemanyag;
+            Ar = ar;
+        }
+
+        public string Marka { get => _marka; set => _marka = value; }
+        public string Modell { get => _modell; set => _modell = value; }
+        public int Ev { get => _ev; set => _ev = value; }
+        public int Kilometer { get => _kilometer; set => _kilometer = value; }
+        public string Uzemanyag { get => _uzemanyag; set => _uzemanyag = value; }
+        public int Ar { get => _ar; set => _ar = value; }
+
+        public override string ToString()
+        {
+            return _marka + _modell + _ev + _kilometer + _uzemanyag + _ar;
+        }
+    }
+}
---
+++ Git+Class2/Beolvas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Git_Class2
+{
+    class Beolvas
+    {
+
+        List<Auto> Autok = new List<Auto>();
+
+     
+        public void autoBeolvas()
+        {
+            StreamReader olvas = new StreamReader("auto.csv");
+            string jelenlegisor;
+            while (!olvas.EndOfStream)
+            {
+
+            jelenlegisor = olvas.ReadLine();
+            }
+            
+        }
+
+
+    }
+}
